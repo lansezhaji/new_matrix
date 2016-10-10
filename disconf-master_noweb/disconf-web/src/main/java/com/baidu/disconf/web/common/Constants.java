2a6b12939bd25e5c737395014cc45e136aa341fd
gaojian@danlu.com <gaojian@danlu.com>
2016-10-10 19:44:14 +0800

    修改jdbc文件

--- disconf-master_noweb/disconf-web/src/main/java/com/baidu/disconf/web/common/Constants.java
+++ disconf-master_noweb/disconf-web/src/main/java/com/baidu/disconf/web/common/Constants.java
@@ -7,6 +7,11 @@ public class Constants {
 
     public final static Integer STATUS_NORMAL = 1;
     public final static Integer STATUS_DELETE = 0;
+    
+    /**
+     * redmine的一些信息
+     * add by GaoJean  on 10/10/2016
+     */
     public final static String Redmine_URL = "http://182.92.230.201:3000/issues/6186.json";
     public final static String Redmine_User_KEY = "1d893519a4674aa4644704c3f8c86003249db597";
     
